0191ccdd4bfa290d05e5fcfb24525d0c394b933c
Suyash R <suyash14cs@gmail.com>
2024-09-20 17:02:15 +0530

    Add files via upload

---
+++ hollow_half_pyramid.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%i",&rows);
+
+    for (int i=1; i<=rows; i++){
+        for (int j=1; j<=i; j++){
+            if (i!=rows){
+                if (j==1 || j==i){
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}

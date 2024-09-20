b04a62cb897e577f82625696009648bf05b4437f
Suyash R <suyash14cs@gmail.com>
2024-09-20 19:01:12 +0530

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
     int rows;
     printf("Enter the number of rows: ");
     scanf("%i",&rows);
 
-    for (int i=1; i<=rows; i++){
-        for (int j=rows-1; j>=i; j--){
+    for (int i=1; i<=rows; i++)
+    {
+        for (int j=rows-1; j>=i; j--)
+        {
             printf(" ");
         }
-        for (int k=2*i-1; k>=1; k--){
+        for (int k=2*i-1; k>=1; k--)
+        {
             printf("*");
         }
         printf("\n");

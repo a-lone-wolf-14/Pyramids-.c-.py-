ed712ac6519d0dc9c6c31e3d6623c1c44852d3a1
Suyash R <suyash14cs@gmail.com>
2024-09-20 10:50:11 +0530

    Add files via upload

---
+++ inverted_pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%i",&rows);
+
+    for (int i=1; i<=rows; i++){
+        for (int j=1; j<i; j++){
+            printf(" ");
+        }
+        for (int k=2*rows-1; k>=2*i-1; k--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
